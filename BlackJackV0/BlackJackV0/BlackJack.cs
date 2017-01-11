229fd2947e83fdedaed11d284dc9ca342c1dbdb6
K1539 <K1539@ICT-421-WS-29>
2017-01-11 15:40:11 +0200

    moi

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -1,34 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 /// <summary>
-/// A simple example of Blackjack card game 
+/// A simple example of Blackjack card game
 /// </summary>
+/// /*
+/// 1) Muuta ohjelmaa että käyttäjältä kysytään lukua niin kauan että se on annetulta väliltä 
+/// 2) Tee ohjelmaan muutos että käyttäjä voi pelata niin kauan kunnes hän antaa syötteeksi: X tai exit. 
+///     Jos käyttäjä antaa jonkin muun merkkijonon joka ei ole kokonaisluku käyttäjälle näytetään ohje sallituista syötteistä. 
+/// 3) Tee muutos että myös pöydän korttien arvo arvotaan väliltä 10-21. Näytä arvottu luku tuloksen yhteydessä.
+/// */
+
 namespace JAMK.IT
 {
-  class BlackJack
-  {
-    static void Main()
+    class Peli
     {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
+        static void Main()
         {
-          System.Console.WriteLine("You win.");
+            Console.Write("Haluatko pelata BlackJackia (1) vai Lottoa (2)? Valitsemalla 0 Peli loppuu. > ");
+            int Valinta = int.Parse(Console.ReadLine());
+
+            if (Valinta == 1)
+            {
+                BlackJack();
+            }
+            if (Valinta == 2)
+            {
+                JAMK.IT.Lotto.Lottopeli();
+            }
         }
-        else {
-          System.Console.WriteLine("You lose.");
+
+
+
+
+        static void BlackJack()
+        {
+            int myNumber;
+            int theirNumber;
+            string UserInput;
+            bool isNumeric;
+            Random rnd = new Random();
+            myNumber = rnd.Next(10, 21);
+            while (true)
+            {
+                System.Console.WriteLine("*** BlackJack! ***");
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+                UserInput = System.Console.ReadLine();
+                //comparing that given umber is valid
+                isNumeric = int.TryParse(UserInput, out theirNumber);
+                if (isNumeric == true)
+                {
+                    if (theirNumber < 1 || theirNumber > 21)
+                    {
+                        Console.WriteLine("The given number is out of limits, try again.");
+                    }
+                    else
+                    {
+                        //comparing
+                        if (theirNumber >= myNumber && theirNumber <= 21)
+                        {
+                            System.Console.WriteLine("You win.");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("You lose.");
+                        }
+                    }
+                }
+                else if (UserInput == "X" || UserInput == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Virheellinen Valinta!\n" +
+                                "Ohjelmaan käy vain:\n" +
+                                "\tnumerot 1-21\n" +
+                                "\tX & exit ohjelmasta poistumiseen\n");
+                }
+            }
         }
-      }
     }
-  }
 }
